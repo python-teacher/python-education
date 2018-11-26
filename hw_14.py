9aae36028ccfa408133312be0e9f4c7fed4fd043
Sergiy Budz <budzdsergiy777@gmail.com>
2018-11-27 00:14:21 +0200

    Initial commit

---
+++ hw_14.py
@@ -0,0 +1,24 @@
+class Car:
+    def __init__(self, type, model, year, max_speed, ):
+        self.type = type
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+
+    def information_car(self):
+        car = str(self.type) + ' ' + str(self.model) + ' ' + str(self.year) + ' ' + str(self.max_speed)
+        return car
+
+    def get_car_type(self):
+        return self.type
+
+    def change_type(self, types):
+        self.type = types
+
+
+car = Car('gas', 'BMW', 2018, 280)
+print(car.get_car_type())  # prints `Gas`
+car.change_type('petrol')
+print(car.get_car_type())  # prints `Petrol`.
+car.change_type('electrical')
+print(car.get_car_type())  # prints `Electrical`.
