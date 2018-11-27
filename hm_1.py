c47e847b5a9109a227d78871958524e439163bf7
Sergiy Budz <budzdsergiy777@gmail.com>
2018-11-27 19:41:10 +0200

    Renamed and added variables.

--- hm_1.py
+++ hm_1.py
@@ -1,5 +1,24 @@
-num = input('Num: ')
-suma = 0
-for i in num:
-    suma += int(i)
-print9(suma)
+"""
+Calculate sum of all digits of a number set in string.
+
+Example:
+
+num = С12345Т
+
+Your script should calculate it like: 1+2+3+4+5 = 15
+
+num = С611091234512Т
+result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
+"""
+def sum_str(n):
+    sum_line = 0
+    for i in n:
+        sum_line += int(i)
+    print("—ума строки {n} = {sum_line}".format(n=n,sum_line=sum_line) )
+
+first_example = "93134053241522520541"
+second_example = "6141232523952344"
+third_example = "987654321123456789"
+sum_str(first_example)
+sum_str(second_example)
+sum_str(third_example)
