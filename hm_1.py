2fe00466a543225943947522c1492a0b60d94457
Sergiy Budz <budzdsergiy777@gmail.com>
2018-11-28 10:00:31 +0200

    сhanged 4 lines on 1 line

--- hm_1.py
+++ hm_1.py
@@ -10,15 +10,16 @@ Your script should calculate it like: 1+2+3+4+5 = 15
 num = С611091234512Т
 result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
 """
-def sum_str(n):
-    sum_line = 0
-    for i in n:
-        sum_line += int(i)
-    print("—ума строки {n} = {sum_line}".format(n=n,sum_line=sum_line) )
 
-first_example = "93134053241522520541"
-second_example = "6141232523952344"
+
+def sum_str(n):
+    sum_line = sum(int(i) for i in str(n))
+    return ("Suma line {n} = {sum_line}".format(n=n, sum_line=sum_line))
+
+
+first_example = "931340532415225205491"
+second_example = "61412325234952344"
 third_example = "987654321123456789"
-sum_str(first_example)
-sum_str(second_example)
-sum_str(third_example)
+print(sum_str(first_example))
+print(sum_str(second_example))
+print(sum_str(third_example))
