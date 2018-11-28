bfb030441af4533730dcffd1551dc6a86736cec2
Sergiy Budz <budzdsergiy777@gmail.com>
2018-11-28 19:45:38 +0200

    Corrected error detection for the letter

--- hm_1.py
+++ hm_1.py
@@ -13,13 +13,15 @@ result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
 
 
 def sum_str(n):
-    sum_line = sum(int(i) for i in str(n))
+    sum_line = sum(int(i) for i in str(n) if i.isdigit())
     return ("Suma '{}' = {}".format(n,sum_line))
 
 
 first_example = "931340532415225205491"
 second_example = "61412325234952344"
 third_example = "987654321123456789"
+fourth_example = "999F9999saq999X91QBF"
 print(sum_str(first_example))
 print(sum_str(second_example))
 print(sum_str(third_example))
+print(sum_str(fourth_example))
