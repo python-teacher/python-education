370cfaa75d478a681d8246d0bfdd90bc2c0378b5
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-04 16:52:44 +0200

    updated name, docstring added

--- hw_5.py
+++ hw_5.py
@@ -1,5 +1,12 @@
-x = int(input('Введіть скільки останніх рядків відобразити?: '))
-with open('file/file.txt' , 'r') as f:
-    lines = f.readlines()[-x:]
-    for line in lines:
-        print(line)
+﻿"""
+Write a program to read last n lines of a file.
+"""
+
+
+def read_last_N_lines():
+    with open('file/file.txt', 'r') as f:
+        x = int(input('Введіть скільки останніх рядків відобразити?: '))
+        lines = f.readlines()[-x:]
+        for line in lines:
+            print(line)
+read_last_N_lines()
