e9a6d56e1432a424e08bd33a22fcd62defc87651
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-04 18:48:20 +0200

    updated name, docstring added

--- hw_8.py
+++ hw_8.py
@@ -1,8 +1,14 @@
-def create_dict(N):
-    sqrt_dict = {i: i ** 2 for i in range(1, N + 1)}
-    return sqrt_dict
+"""
+Write a function which returns a dictionary with the following format:
+return_square_dict(N)
+
+{1:1, 2:4, 3:9, 4:16, 5:25, 6:36, 7:49, 8:64, 9:81, …, N:N^2}
+"""
 
 
-print(create_dict(20))
-print(create_dict(40))
-print(create_dict(100))
+def return_square_dict(N):
+    return {i: i ** 2 for i in range(1, N + 1)}
+
+
+print(return_square_dict(20))
+print(return_square_dict(100))
