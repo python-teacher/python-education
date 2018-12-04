8fc285f6f78b01a6a6b66a2e55134c79d59c221f
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-04 16:35:33 +0200

    updated name, docstring added

--- hm_4.py
+++ hm_4.py
@@ -1,4 +1,12 @@
+"""
+Write a Python program to get the current time in Python.
+"""
+
 import time
-def clock():
+
+
+def return_time():
     return time.ctime()
-print(clock())
+
+
+print(return_time())
