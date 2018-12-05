9f946a3c63387dd839af2889285a852b990db112
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-06 00:32:36 +0200

    update return

--- hw_5.py
+++ hw_5.py
@@ -5,6 +5,5 @@ Write a program to read last n lines of a file.
 
 def read_last_N_lines():
     with open('file/short documentation.txt', 'r') as f:
-        lines = f.readlines()[-5:]
-        return lines
+        return f.readlines()[-5:]
 print(read_last_N_lines())
