3bf7d65e3af1593d7b76ce6b6418590e82c6b68f
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-05 10:52:33 +0200

    print  replaced by a return

--- hw_5.py
+++ hw_5.py
@@ -7,6 +7,5 @@ def read_last_N_lines():
     with open('file/file.txt', 'r') as f:
         x = int(input('Введіть скільки останніх рядків відобразити?: '))
         lines = f.readlines()[-x:]
-        for line in lines:
-            print(line)
-read_last_N_lines()
+        return lines
+print(read_last_N_lines())
