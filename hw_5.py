b5bb1ae43342d88c727044f3a5c4eb7627d5d59d
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-07 19:35:59 +0200

    the function accepts the parameters

--- hw_5.py
+++ hw_5.py
@@ -3,7 +3,7 @@ Write a program to read last n lines of a file.
 """
 
 
-def read_last_N_lines():
+def read_last_N_lines(n):
     with open('file/short documentation.txt', 'r') as f:
-        return f.readlines()[-5:]
-print(read_last_N_lines())
+        return f.readlines()[-n:]
+print(read_last_N_lines(10))
