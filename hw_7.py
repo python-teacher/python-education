b74a2f70233e3b659ed1c1bbd6de9fce732b1c12
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-07 20:09:36 +0200

    updated algorithm for counting lines

--- hw_7.py
+++ hw_7.py
@@ -4,8 +4,7 @@ Write a program to count the number of lines in a text file.
 
 
 def count_rows():
-    with open('documentation file/short documentation.txt', 'r') as f:
-        return sum(1 for x in f)
+    return len(open('file/short documentation.txt', 'r').readlines())
 
 
 print(count_rows())
