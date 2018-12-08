7695f906401b8470ac345e549304fa5f850ea4d2
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-08 19:23:58 +0200

    simplify the code

--- hw_7.py
+++ hw_7.py
@@ -4,7 +4,9 @@ Write a program to count the number of lines in a text file.
 
 
 def count_rows():
-    return len(open('file/short documentation.txt', 'r').readlines())
+    with open('file/short documentation.txt', 'r') as f:
+        line = f.readlines()
+        return len(line)
 
 
 print(count_rows())
