96859e9ce10cff98a6f70fa12bddc922e3e66a7c
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-08 19:27:17 +0200

    function renamed

--- hw_5.py
+++ hw_5.py
@@ -3,7 +3,9 @@ Write a program to read last n lines of a file.
 """
 
 
-def read_last_N_lines(n):
+def read_last_lines(n):
     with open('file/short documentation.txt', 'r') as f:
         return f.readlines()[-n:]
-print(read_last_N_lines(10))
+
+
+print(read_last_lines(10))
