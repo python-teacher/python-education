9b5995974eee1d0f06a87e0f5fe240acc35174dc
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-09 12:35:49 +0200

    renamed file and variable, and deleted the folder

--- hw_7.py
+++ hw_7.py
@@ -4,9 +4,9 @@ Write a program to count the number of lines in a text file.
 
 
 def count_rows():
-    with open('file/short documentation.txt', 'r') as f:
-        line = f.readlines()
-        return len(line)
+    with open('testing_data.txt', 'r') as f:
+        lines = f.readlines()
+        return len(lines)
 
 
 print(count_rows())
