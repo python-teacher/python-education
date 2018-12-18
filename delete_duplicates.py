928423fb8691f0ccac8f481170d2ed647dfd5b6e
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-18 11:38:20 +0200

    renamed file and add docstring

---
+++ delete_duplicates.py
@@ -0,0 +1,13 @@
+"""
+With a given list [12,24,35,24,88,120,155,88,120,155], write a program to
+print this list after removing all duplicate 
+values with original order reserved.
+"""
+
+
+def original_numbers():
+    repeating_values = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
+    print(list(set(repeating_values))[::-1])
+
+
+original_numbers()
--- hw_11.py
+++
@@ -1,5 +0,0 @@
-def func():
-    repeating_values = [12,24,35,24,88,120,155,88,120,155]
-    print(repeating_values)
-    print(list(set(repeating_values))[::-1])
-func()
