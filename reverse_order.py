bff0ef24b5d9abe56e70fb4d8eae8514bedfd219
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-18 11:45:05 +0200

    renamed file and add docstring

---
+++ reverse_order.py
@@ -0,0 +1,15 @@
+"""
+Write a function which prints a sentence in a reverse order.
+Input example:
+This is a test sentence
+
+Output example:
+sentences test a si sihT
+"""
+
+
+def reverse_line(word):
+    return word[::-1]
+
+
+print(reverse_line('This is a test sentence'))
