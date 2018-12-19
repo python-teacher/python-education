6cbe0aab9abaea166ce33d0c21151cf625a5fa7c
Sergiy Budz <budzdsergiy777@gmail.com>
2018-12-19 22:20:49 +0200

    replaced the receiving function

--- delete_duplicates.py
+++ delete_duplicates.py
@@ -5,8 +5,9 @@ values with original order reserved.
 """
 
 
-def original_numbers(*args):
-    return list(set(args))[::-1]
+def original_numbers(numbers):
+    return list(set(numbers))[::-1]
 
 
-print(original_numbers(12, 24, 35, 24, 88, 120, 155, 88, 120, 155))
+
+print(original_numbers([12, 24, 35, 24, 88, 120, 155, 88, 120, 155]))
