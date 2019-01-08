6de39f7ea0cd7bdd57c3352f00b7853cc668816b
Sergiy Budz <budzdsergiy777@gmail.com>
2019-01-08 15:20:32 +0200

    updated code for different versions of the python

--- squares_of_numbers.py
+++ squares_of_numbers.py
@@ -10,17 +10,27 @@ Your new list should looks like:
 
 Please print values from the second list.
 
-P.S. This task might look super easy, but there are a few cases. I guess, you’ll find them very quickly.
-Your code should be able to execute on very simple computer(without huge amount of virtual memory, CPU, etc.)
+P.S. This task might look super easy, but there are a few cases. I guess, 
+you’ll find them very quickly.
+Your code should be able to execute on very simple computer(without huge 
+amount of virtual memory, CPU, etc.)
 Your code should works under both versions of Python (2.X, 3.X)
 
 """
 
 
+def z_range(n):
+    i = 0
+    while i < n:
+        yield i
+        i += 1
+
+
 def squares_of_numbers():
-    list_of_numbers = (x for x in range(0, 100000000000))
-    new_squares_list= (i * i for i in list_of_numbers)
-    for concatenation in new_squares_list:
-        print(concatenation)
+    list_of_numbers = (x for x in z_range(100000000000))
+    new_squares_list = (i * i for i in list_of_numbers)
+    for squared in new_squares_list:
+        print(squared)
+
 
 squares_of_numbers()
