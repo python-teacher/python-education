c94806db6c1a8c65444d34a2bd31e56e7d308257
Sergiy <budzsergiy777@gmail.com>
2019-01-24 23:52:54 +0200

    working

--- car.py
+++ car.py
@@ -13,18 +13,24 @@ gas_car = car.set_car_type('gas')  # It returns `GasCar` instance.
 
 
 class Car:
-    def __init__(self, model, year, max_speed, types=None):
+    def __init__(self, model, year, max_speed):
         self.model = model
         self.year = year
         self.max_speed = max_speed
-        self.types = types
 
-    def set_car_type(self, types):
-        self.types = types.title() + 'Car'
-        return Car(self.model, self.year, self.max_speed, self.types)
-
-    def get_car_type(self):
-        return self.types
+    def set_car_type(self, new_type):
+        if new_type == 'petrol':
+            return PetrolCar(self.model, self.year, self.max_speed)
+        elif new_type == 'gas':
+            return GasCar(self.model, self.year, self.max_speed)
+        elif new_type == 'electron':
+            return ElectronCar(self.model, self.year, self.max_speed)
+            # subcl =  self.__class__.__subclasses__()
+            # print(subcl)
+            # for cl in subcl:
+            #     print(cl)
+            #     if cl.types == subcl:
+            #         return cl(self.model, self.year, self.max_speed)
 
     def get_model(self):
         return self.model
@@ -36,13 +42,37 @@ class Car:
         return self.max_speed
 
 
+class GasCar(Car):
+    types = 'Gas'
+
+    def __init__(self, model, year, max_speed):
+        super().__init__(model, year, max_speed)
+
+
+class ElectronCar(Car):
+    types = 'Electron'
+
+    def __init__(self, model, year, max_speed):
+        super().__init__(model, year, max_speed)
+
+
+class PetrolCar(Car):
+    types = 'Petrol'
+
+    def __init__(self, model, year, max_speed):
+        super().__init__(model, year, max_speed)
+
+
 car = Car('BMW', 2018, 300)
 petrol_car = car.set_car_type('petrol')  # It returns `PetrolCar` instance.
 
-print(petrol_car.get_car_type())
 print(petrol_car.get_model())  # Returns `BMW`
 print(petrol_car.get_year())  # Returns 2018
 print(petrol_car.get_max_speed())  # Returns 300
 
 gas_car = car.set_car_type('gas')  # It returns `GasCar` instance.
-print(gas_car.get_car_type())
+print(gas_car.__class__)  # <class '__main__.GasCar'>
+print(petrol_car.__class__)  # <class '__main__.PetrolCar'>
+print(isinstance(petrol_car, PetrolCar))  # True
+print(isinstance(gas_car, GasCar))  # True
+print(isinstance(gas_car, ElectronCar))  # False
