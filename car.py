010a9a35e42255900e77a9b51e39f37060455663
Sergiy <budzsergiy777@gmail.com>
2019-01-25 18:50:35 +0200

    add dict

--- car.py
+++ car.py
@@ -19,12 +19,14 @@ class Car:
         self.max_speed = max_speed
 
     def set_car_type(self, new_type):
-        if new_type == 'petrol':
-            return PetrolCar(self.model, self.year, self.max_speed)
-        elif new_type == 'gas':
-            return GasCar(self.model, self.year, self.max_speed)
-        elif new_type == 'electron':
-            return ElectronCar(self.model, self.year, self.max_speed)
+        mapped_classes = {
+            "petrol": PetrolCar,
+            "gas": GasCar,
+            "electro": ElectronCar
+        }
+        for key, value in mapped_classes.items():
+            if new_type in key:
+                return value(self.model, self.year, self.max_speed)
 
     def get_model(self):
         return self.model
@@ -59,7 +61,6 @@ class PetrolCar(Car):
 
 car = Car('BMW', 2018, 300)
 petrol_car = car.set_car_type('petrol')  # It returns `PetrolCar` instance.
-
 print(petrol_car.get_model())  # Returns `BMW`
 print(petrol_car.get_year())  # Returns 2018
 print(petrol_car.get_max_speed())  # Returns 300
