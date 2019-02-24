13e3b7278c5a1931bea097ca6d7bbaa3b1598fd9
Sergiy Budz <budzsergiy777@gmail.com>
2019-02-25 00:35:15 +0200

    deleted line

--- get_instagram.py
+++ get_instagram.py
@@ -3,27 +3,20 @@ from key import *
 import requests
 
 
-def get_user_id(username):
+def user(username):
 	request = requests.get(userID + str(username)).json()
-	userid = 0
+	user_id = 0
 	for i in request['users']:
-		userid = int(i['user']['pk'])
-	return userid
-
-
-def user(user_id):
+		user_id = int(i['user']['pk'])
 	api = InstagramAPI(username=user_name, password=password)
 	api.login()
 	followers = api.getTotalFollowers(user_id)
 	followings = api.getTotalFollowings(user_id)
-
 	print(f'Number of followers: {len(followers)}!')
 	print(f'Number of followings: {len(followings)}!')
-
 	for following in followings:
 		print(f"{urlInstagram}{following['username']}")
 
 
 if __name__ == '__main__':
-	ID = get_user_id('case_iphone_lviv2019')
-	user(ID)
+	user('sergiy.budz')
--- key.py
+++ key.py
@@ -1,7 +1,4 @@
-urlInstagram = "https://www.instagram.com/"
-
-APP_TOKEN = '11166443968.c11ca5e.3cbeb9018cd249a589cace56eaaff7e4'
-userID = "https://www.instagram.com/web/search/topsearch/?context=user&count=0&query="
+﻿userID = "https://www.instagram.com/web/search/topsearch/?context=user&count=0&query="
 
 user_name = 'case_iphone_lviv2019'
 password = 'игвяеуіе123'
