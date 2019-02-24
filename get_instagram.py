2f572809552fa1383ad3619061f04d74883b9361
Sergiy Budz <budzsergiy777@gmail.com>
2019-02-24 16:34:56 +0200

    new

--- get_instagram.py
+++ get_instagram.py
@@ -1,27 +1,20 @@
-import requests
-
-BASE_URL = 'https://api.instagram.com/v1/'
-APP_TOKEN = '11166443968.c11ca5e.3cbeb9018cd249a589cace56eaaff7e4'
+from InstagramAPI import InstagramAPI
+from key import *
 
 
-def get_user():
-	request_url = (f"{BASE_URL}users/self/?access_token={APP_TOKEN}")
-	good_link = requests.get(request_url).json()
-	return good_link['data']['counts']
+def user():
+	api = InstagramAPI(username=user_name, password=password)
+	api.login()
+	user_id = 9755530901  # 
+	followers = api.getTotalFollowers(user_id)
+	followings = api.getTotalFollowings(user_id)
+
+	print(f'Number of followers: {len(followers)}!')
+	print(f'Number of followings: {len(followings)}!')
+
+	for following in followings:
+		print(f"{urlInstagram}{following['username']}")
 
 
-def get_post():
-	request_url = (
-		f"{BASE_URL}users/self/media/recent/?access_token={APP_TOKEN}\n")
-	print(f'Request data from {request_url}')
-	recent_post = requests.get(request_url).json()
-	for data in recent_post['data']:
-		print(f"Like count post {data['likes']['count']} and comment post "
-			  f"{data['comments']['count']}")
-		print(f'Tags {data["caption"]["text"]}')
-		print(data['images']['standard_resolution']['url'])
-		print()
-
-
-print(get_user())
-get_post()
+if __name__ == '__main__':
+	user()
