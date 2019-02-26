6e2a39b6aaabedac3af266c916f812fcb3eabfeb
Sergiy Budz <budzsergiy777@gmail.com>
2019-02-26 23:30:16 +0200

    updated id

--- get_instagram.py
+++ get_instagram.py
@@ -1,29 +1,24 @@
 from InstagramAPI import InstagramAPI
 from key import *
-import requests
 
 
-def get_user_id(username):
-	request = requests.get(userID + str(username)).json()
-	userid = 0
-	for i in request['users']:
-		userid = int(i['user']['pk'])
-	return userid
-
-
-def user(user_id):
+def user(username):
 	api = InstagramAPI(username=user_name, password=password)
 	api.login()
-	followers = api.getTotalFollowers(user_id)
-	followings = api.getTotalFollowings(user_id)
-
-	print(f'Number of followers: {len(followers)}!')
-	print(f'Number of followings: {len(followings)}!')
-
-	for following in followings:
-		print(f"{urlInstagram}{following['username']}")
+	try:
+		api.searchUsername(username)
+		user_id = api.LastJson["user"]["pk"]
+		print(user_id)
+		followers = api.getTotalFollowers(user_id)
+		followings = api.getTotalFollowings(user_id)
+		print(f'Number of followers: {len(followers)}!')
+		print(f'Number of followings: {len(followings)}!')
+		for following in followings:
+			print(f"{urlInstagram}{following['username']}")
+	except Exception:
+		print("Username doesn't exist")
+		return False
 
 
 if __name__ == '__main__':
-	ID = get_user_id('case_iphone_lviv2019')
-	user(ID)
+	user('case_iphone_lviv2019')
--- key.py
+++ key.py
@@ -1,5 +1,3 @@
-﻿userID = "https://www.instagram.com/web/search/topsearch/?context=user&count=0&query="
-
-user_name = 'case_iphone_lviv2019'
+﻿user_name = 'case_iphone_lviv2019'
 password = 'игвяеуіе123'
 
