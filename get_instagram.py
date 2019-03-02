0557fd0e440775735c435ff54062dc5726fbd0ed
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-02 20:05:35 +0200

    added docstring

--- get_instagram.py
+++ get_instagram.py
@@ -4,25 +4,21 @@ from key import user_name, password
 
 class GetInstagramInfo:
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.api = InstagramAPI(username=user_name, password=password)
-        self.api.login()
-        self.user_id = self.api.username_id
+	self.username = username
+	self.password = password
+	self.api = InstagramAPI(username=user_name, password=password)
+	self.api.login()
+	self.user_id = self.api.username_id
 
     def get_followers(self):
-        followers_list = list()
-        followers = self.api.getTotalFollowers(self.user_id)
-        for follower in followers:
-            followers_list.append(follower['username'])
-        return followers_list
+	"""Demonstrate followers of the profile"""
+	followers = self.api.getTotalFollowers(self.user_id)
+	return [follower['username'] for follower in followers]
 
     def get_followings(self):
-        followings_list = list()
-        followings = self.api.getTotalFollowings(self.user_id)
-        for following in followings:
-            followings_list.append(following['username'])
-        return followings_list
+	"""Demonstrate the following profile"""
+	followings = self.api.getTotalFollowings(self.user_id)
+	return [following['username'] for following in followings]
 
 
 if __name__ == '__main__':
