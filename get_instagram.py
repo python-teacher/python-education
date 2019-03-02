bd05ad6e7db21839691ef20e3f0d3f20600d16b1
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-02 14:01:05 +0200

    updated init

--- get_instagram.py
+++ get_instagram.py
@@ -6,23 +6,22 @@ class GetInstagramInfo:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-    api = InstagramAPI(username=user_name, password=password)
-    api.login()
-    user_id = api.username_id
+        self.api = InstagramAPI(username=user_name, password=password)
+        self.api.login()
+        self.user_id = self.api.username_id
 
     def get_followers(self):
         followers_list = list()
         followers = self.api.getTotalFollowers(self.user_id)
         for follower in followers:
-            followers_list.append(f"{follower['username']}")
+            followers_list.append(follower['username'])
         return followers_list
 
     def get_followings(self):
         followings_list = list()
         followings = self.api.getTotalFollowings(self.user_id)
         for following in followings:
-            followings_list.append(f"{following['username']}")
+            followings_list.append(following['username'])
         return followings_list
 
 
