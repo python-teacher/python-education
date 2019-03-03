20c81f181a5e6dca6ca64fe06810f6cac98a4613
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-03 16:07:41 +0200

    deleted spaces

--- get_instagram.py
+++ get_instagram.py
@@ -3,27 +3,27 @@ from key import user_name, password
 
 
 class GetInstagramInfo:
-	def __init__(self, username, password):
-		self.username = username
-		self.password = password
-		self.api = InstagramAPI(username=user_name, password=password)
-		self.api.login()
-		self.user_id = self.api.username_id
+    def __init__(self, username, password):
+	self.username = username
+	self.password = password
+	self.api = InstagramAPI(username=user_name, password=password)
+	self.api.login()
+	self.user_id = self.api.username_id
 
-	def get_followers(self, user_id=None):
-		"""Returns list of user's followers."""
-		user_id = user_id or self.api.username_id
-		followers = self.api.getTotalFollowers(user_id)
-		return [follower['username'] for follower in followers]
+    def get_followers(self, user_id=None):
+	"""Returns list of user's followers."""
+	user_id = user_id or self.api.username_id
+	followers = self.api.getTotalFollowers(user_id)
+	return [follower['username'] for follower in followers]
 
-	def get_followings(self, user_id=None):
-		"""Returns list of user's followings."""
-		user_id = user_id or self.api.username_id
-		followings = self.api.getTotalFollowings(user_id)
-		return [following['username'] for following in followings]
+def get_followings(self, user_id=None):
+    	"""Returns list of user's followings."""
+	user_id = user_id or self.api.username_id
+	followings = self.api.getTotalFollowings(user_id)
+	return [following['username'] for following in followings]
 
 
 if __name__ == '__main__':
-	instagram_info = GetInstagramInfo(user_name, password)
-	print(instagram_info.get_followers())
-	print(instagram_info.get_followings())
+    instagram_info = GetInstagramInfo(user_name, password)
+    print(instagram_info.get_followers())
+    print(instagram_info.get_followings())
