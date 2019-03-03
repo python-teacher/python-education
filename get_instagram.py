bcc667f745ac722588efc90e66af0ef1d8ba05ff
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-03 10:38:49 +0200

    added id=None

--- get_instagram.py
+++ get_instagram.py
@@ -3,25 +3,27 @@ from key import user_name, password
 
 
 class GetInstagramInfo:
-    def __init__(self, username, password):
-	self.username = username
-	self.password = password
-	self.api = InstagramAPI(username=user_name, password=password)
-	self.api.login()
-	self.user_id = self.api.username_id
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		self.api = InstagramAPI(username=user_name, password=password)
+		self.api.login()
+		self.user_id = self.api.username_id
 
-    def get_followers(self):
-	"""Demonstrate followers of the profile"""
-	followers = self.api.getTotalFollowers(self.user_id)
-	return [follower['username'] for follower in followers]
+	def get_followers(self, user_id=None):
+		"""Returns list of user's followers."""
+		self.user_id = user_id if user_id is not None else self.user_id
+		followers = self.api.getTotalFollowers(self.user_id)
+		return [follower['username'] for follower in followers]
 
-    def get_followings(self):
-	"""Demonstrate the following profile"""
-	followings = self.api.getTotalFollowings(self.user_id)
-	return [following['username'] for following in followings]
+	def get_followings(self, user_id=None):
+		"""Returns list of user's followings."""
+		self.user_id = user_id if user_id is not None else self.user_id
+		followings = self.api.getTotalFollowings(self.user_id)
+		return [following['username'] for following in followings]
 
 
 if __name__ == '__main__':
-    instagram_info = GetInstagramInfo(user_name, password)
-    print(instagram_info.get_followers())
-    print(instagram_info.get_followings())
+	instagram_info = GetInstagramInfo(user_name, password)
+	print(instagram_info.get_followers())
+	print(instagram_info.get_followings())
