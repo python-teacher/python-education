7deb413d51e22b654deb6cebb48fd05eaadad58e
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-13 19:59:39 +0200

    added to save in database

--- search_username/catalog/models.py
+++ search_username/catalog/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-# Create your models here.
+Base = declarative_base()
+
+
+class SaveDataBase(Base):
+    __tablename__ = "Instagram"
+
+    id = Column(Integer, primary_key=True)
+    followers = Column(String(30))
+    followings = Column(String(30))
+    time = Column(String)
--- search_username/catalog/views.py
+++ search_username/catalog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from InstagramAPI import InstagramAPI
 from search_username.settings import password, user_name
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import SaveDataBase, Base
+import datetime
+import itertools
+
+engine = create_engine('sqlite:///users.db')
 
 
 class GetInstagramInfo:
@@ -13,34 +20,48 @@ class GetInstagramInfo:
         self.api.searchUsername(name)
         return self.api.LastJson["user"]["pk"]
 
-    def get_followers(self, user_id=None):
+    def get_followers(self, user_id):
         """Returns list of user's followers."""
-        user_id = user_id or self.api.username_id
         followers = self.api.getTotalFollowers(user_id)
         return [follower['username'] for follower in followers]
 
-    def get_followings(self, user_id=None):
+    def get_followings(self, user_id):
         """Returns list of user's followings."""
-        user_id = user_id or self.api.username_id
         followings = self.api.getTotalFollowings(user_id)
         return [following['username'] for following in followings]
 
 
 instagram_info = GetInstagramInfo(user_name, password)
+user = None
 
 
 def index(request):
-    user = None
     if request.method == "GET":
         name = request.GET.get('name', '')
         if name:
             user_id = instagram_info.get_user_id(name)
             followers = instagram_info.get_followers(user_id)
             followings = instagram_info.get_followings(user_id)
+            global user
             user = dict(
                 list_of_followers=followers,
                 list_of_followings=followings,
                 name=name)
+        if request.GET.get('save') == '':
+            Base.metadata.create_all(engine)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            for (followings, followers) in itertools.zip_longest(
+                    user['list_of_followings'],
+                    user['list_of_followers'],
+                    fillvalue=' '):
+                text = SaveDataBase(followings=followings,
+                                    followers=followers,
+                                    time=str(now))
+                session.add(text)
+            session.commit()
+            return render(request, "index.html")
         return render(request, "index.html", context=user)
     else:
         return render(request, "index.html")
