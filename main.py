a699840928580c4335af576e4dc3b70700f9c75f
Sergiy Budz <budzsergiy777@gmail.com>
2019-03-23 20:08:01 +0200

    added logic

--- main.py
+++ main.py
@@ -40,56 +40,57 @@ class GetInstagramInfo:
     def time_now(self):
         return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-    def save_db(self, followers, followings):
+    def save_db(self, connect_database, followers, followings):
         """Written followers and followings user to database"""
-        session = self.create_database()
+        session = connect_database
         now = self.time_now()
         for (followings, followers) in itertools.zip_longest(followings, followers, fillvalue=''):
             text = InstagramUsers(followings=followings, followers=followers, status="new", time=str(now))
             session.add(text)
         session.commit()
 
-    def database_followings(self):
+    def database_followings(self, connect_database):
         """Getting all followings of the database"""
-        session = self.create_database()
+        session = connect_database
         database_followings = session.query(InstagramUsers.followings).all()
         return [y for x in database_followings for y in x]
 
-    def database_follower(self):
+    def database_follower(self, connect_database):
         """Getting all followers of the database"""
-        session = self.create_database()
+        session = connect_database
         database_follower = session.query(InstagramUsers.followers).all()
         return [y for x in database_follower for y in x]
 
-    def subscribed(self, new_followers, new_followings):
+    def subscribed(self, connect_database, new_followers, new_followings):
         """Check whether the current user is in the database.If not, then add(status- subscribed)"""
-        session = self.create_database()
+        session = connect_database
         now = self.time_now()
         for following in new_followings:  # write to database subscribed followings
-            if following not in self.database_followings():
+            if following not in self.database_followings(connect_database):
                 text = InstagramUsers(followings=following, followers='', status="subscribed", time=str(now))
                 session.add(text)
             session.commit()
         for follower in new_followers:  # write to database subscribed followers
-            if follower not in self.database_follower():
+            if follower not in self.database_follower(connect_database):
                 text = InstagramUsers(followings='', followers=follower, status="subscribed", time=str(now))
                 session.add(text)
             session.commit()
 
-    def unsubscribed(self, new_followers, new_followings):
+    def unsubscribed(self, connect_database, new_followers, new_followings):
         """Check whether the current database user is in the current list.If not, then add(status-unsubscribed)"""
-        session = self.create_database()
+        session = connect_database
         now = self.time_now()
-        for following_l in self.database_followings():  # write to database unsubscribed followings
-            if following_l not in new_followings:
+
+        for following_l in self.database_followings(connect_database):  # write to database unsubscribed followings
+            if following_l in new_followings:
                 text = InstagramUsers(followings=following_l, followers='', status="unsubscribed", time=str(now))
                 session.add(text)
-            session.commit()
-        for follower_l in self.database_follower():  # write to database unsubscribed followers
-            if follower_l not in new_followers:
+                session.commit()
+        for follower_l in self.database_follower(connect_database):  # write to database unsubscribed followers
+            if follower_l in new_followers:
                 text = InstagramUsers(followings='', followers=follower_l, status="unsubscribed", time=str(now))
                 session.add(text)
-            session.commit()
+                session.commit()
 
 
 if __name__ == '__main__':
@@ -97,6 +98,14 @@ if __name__ == '__main__':
     user_id = instagram_info.get_user_id('case_iphone_lviv2019')
     followers = instagram_info.get_followers(user_id)
     followings = instagram_info.get_followings(user_id)
-    instagram_info.save_db(followers,followings)  # run once and commented
-    # instagram_info.subscribed(followers, followings)  # to uncomment when bd is created
-    # instagram_info.unsubscribed(followings, followers)  # to uncomment when bd is created
+    connect_db = instagram_info.create_database()  # connect to database
+    try:
+        session = [x for x in connect_db.query(InstagramUsers.status).first()]
+        if session[0] == 'new':
+            instagram_info.subscribed(connect_db, followers, followings)  # to uncomment when bd is created
+            instagram_info.unsubscribed(connect_db, followings, followers)  # to uncomment when bd is created
+        else:
+            print(True)
+            instagram_info.save_db(connect_db,followers,followings)  # run once and commented
+    except TypeError:
+        instagram_info.save_db(connect_db, followers, followings)  # run once and commented
